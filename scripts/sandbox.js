5010b4251a4e1ae06638c169537da42a8acee50a
istamosh <t0pramen19@gmail.com>
2023-03-17 19:37:14 +0700

    adding var, let, and const variable.

--- scripts/sandbox.js
+++ scripts/sandbox.js
@@ -1,5 +1,21 @@
 // adding hello world ads screen
-alert('hello world hehe.');
+// alert('hello world hehe.');
 
 // adding some console log in chrome
 console.log(2+2);
+
+// adding let vars for console
+let age = 28;
+let year = 2023;
+console.log(age, year);
+age = 30; // changing var
+console.log(age, year); // the change is applied
+
+// adding const var
+const maxScore = 100;
+// maxScore = 50; // const var can't be reassigned/overriden
+console.log(maxScore);
+
+// var variable for older browser
+var score = 57;
+console.log(score);
