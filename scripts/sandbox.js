3fa217d10627f531572927b34d4c3ce42eadb906
istamosh <t0pramen19@gmail.com>
2023-03-17 20:41:27 +0700

    added mail example, concat, cherrypick letter, length count, uppercase and lowercase method, and find letter count using indexOf method.

--- scripts/sandbox.js
+++ scripts/sandbox.js
@@ -19,3 +19,26 @@ console.log(maxScore);
 // var variable for older browser
 var score = 57;
 console.log(score);
+
+// concatenate (join) two strings
+let userMail = 'alfi';
+let mailDomain = '@istamosh.com';
+
+let mail = userMail + mailDomain;
+console.log(mail);
+
+// cherrypick letter from string
+console.log(userMail[2]);
+
+// count every letter
+console.log(mail.length);
+
+// change whole string to uppercase
+console.log(mail.toUpperCase());
+
+// store username in Uppercase
+let nameUpper = userMail.toUpperCase();
+console.log(nameUpper);
+
+// find @ in count letter of mail
+console.log(mail.indexOf('@'));
